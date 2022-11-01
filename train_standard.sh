71a03d004ba561d3626996b436463ea6a40e48bf
aheldis <ehsanihani30@yahoo.com>
2022-11-01 16:38:23 +0000

    25000 steps mse loss for kaggle

--- train_standard.sh
+++ train_standard.sh
@@ -2,7 +2,7 @@
 mkdir -p checkpoints
 # python -u train.py --name raft-chairs --stage chairs --validation chairs --gpus 0 1 --num_steps 100000 --batch_size 10 --lr 0.0004 --image_size 368 496 --wdecay 0.0001
 # python -u train.py --name raft-things --stage things --validation sintel --restore_ckpt checkpoints/raft-chairs.pth --gpus 0 1 --num_steps 100000 --batch_size 6 --lr 0.000125 --image_size 400 720 --wdecay 0.0001
-python -u train.py --name cosine-sim --stage sintel --validation sintel --gpus 0 --num_steps 25000 --batch_size 6 --lr 0.000125 --image_size 184 384 --wdecay 0.00001 --gamma=0.85
+python -u train.py --name mse --stage sintel --validation sintel --gpus 0 --num_steps 25000 --batch_size 6 --lr 0.000125 --image_size 184 384 --wdecay 0.00001 --gamma=0.85
 # --cont checkpoints/last.pth
 # python -u train.py --name raft-sintel --stage sintel --validation sintel --gpus 0 --num_steps 25000 --batch_size 6 --lr 0.000125 --image_size 184 384 --wdecay 0.00001 --gamma=0.85
 # python -u train.py --name raft-kitti  --stage kitti --validation kitti --restore_ckpt checkpoints/raft-sintel.pth --gpus 0 1 --num_steps 50000 --batch_size 6 --lr 0.0001 --image_size 288 960 --wdecay 0.00001 --gamma=0.85
