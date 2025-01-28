b899396fa41a6b5397cb5915ba926716459bca9d
Haniyeh Ehsani <45337101+aheldis@users.noreply.github.com>
2025-01-27 17:31:17 -0800

    debug

--- demo.py
+++ demo.py
@@ -35,8 +35,12 @@ def load_image(imfile, transform):
 def viz(args, img1, img2, flo, gt_flo, path, _id):
     img = img1[0].permute(1,2,0).cpu().numpy()
     img2 = img2[0].permute(1,2,0).cpu().numpy()
+    boundary = np.max(img.shape[0], img.shape[1])
     gt_flo = gt_flo[0].permute(1,2,0).cpu().numpy()
+    gt_flo = np.where(gt_flo > boundary, boundary, gt_flo)
     flo = flo[0].permute(1,2,0).cpu().numpy()
+    flo = np.where(flo > boundary, boundary, flo)
+
     
     # map flow to rgb image
     gt_flo = flow_viz.flow_to_image(gt_flo)
