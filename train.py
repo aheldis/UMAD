5a439f97247e80ad2aa94078e74926c3396bdbb7
Haniyeh Ehsani <45337101+aheldis@users.noreply.github.com>
2025-04-29 03:41:29 -0700

    Update train.py

--- train.py
+++ train.py
@@ -151,7 +151,7 @@ def composition_loss(flow_preds1, flow_preds2, flow_preds12, gamma):
         flow_composed = torch.from_numpy(flow_composed).to(dtype=flow_preds1[i].dtype, device=flow_preds1[i].device)
         flow_composed_ls += flow_composed
         i_loss = (flow_composed - flow_preds12[i]).abs()
-        flow_loss += i_weight * (valid[:, None] * i_loss).mean()
+        flow_loss += i_weight * i_loss.mean()
 
     epe = torch.sum((flow_composed_ls[-2] - flow_preds12[-2])**2, dim=1).sqrt()
     epe = epe.view(-1)[valid.view(-1)]
