b2b887a2c6df99ec23840e80e35bc67d3c52f676
Haniyeh Ehsani <45337101+aheldis@users.noreply.github.com>
2025-05-21 00:41:31 -0700

    raft sintel

--- train_standard.sh
+++ train_standard.sh
@@ -4,5 +4,6 @@ mkdir -p checkpoints
 # python -u train.py --name raft-things --stage things --validation sintel --restore_ckpt models/raft-chairs.pth --gpus 0 1 --num_steps 100000 --batch_size 6 --lr 0.000125 --image_size 400 720 --wdecay 0.0001
 # python -u train.py --name cosine-sim --stage sintel --validation sintel --gpus 0 --num_steps 25000 --batch_size 6 --lr 0.000125 --image_size 184 384 --wdecay 0.00001 --gamma=0.85
 # --cont checkpoints/last.pth
-python -u train.py --name raft-sintel --stage sintel --validation sintel --restore_ckpt models/raft-chairs.pth --gpus 0 --num_steps 25000 --batch_size 6 --lr 0.000125 --image_size 184 384 --wdecay 0.00001 --gamma=0.85
+# python -u train.py --name raft-sintel --stage sintel --validation sintel --restore_ckpt models/raft-chairs.pth --gpus 0 --num_steps 25000 --batch_size 6 --lr 0.000125 --image_size 184 384 --wdecay 0.00001 --gamma=0.85
+python -u train.py --name raft-sintel --stage sintel --validation sintel --restore_ckpt models/raft-sintel.pth --gpus 0 --num_steps 25000 --batch_size 6 --lr 0.000125 --image_size 184 384 --wdecay 0.00001 --gamma=0.85
 # python -u train.py --name raft-kitti  --stage kitti --validation kitti --restore_ckpt models/raft-sintel.pth --gpus 0 1 --num_steps 50000 --batch_size 6 --lr 0.0001 --image_size 288 960 --wdecay 0.00001 --gamma=0.85
