27c290191f062e8076951b182791bde6687fdf13
Haniyeh Ehsani <45337101+aheldis@users.noreply.github.com>
2025-06-26 17:06:25 -0700

    debug

--- core/datasets.py
+++ core/datasets.py
@@ -288,10 +288,13 @@ def fetch_dataloader(args, TRAIN_DS='C+T+K+S+H'):
         if TRAIN_DS == 'C+T+K+S+H':
             kitti = KITTI({'crop_size': args.image_size, 'min_scale': -0.3, 'max_scale': 0.5, 'do_flip': True})
             hd1k = HD1K({'crop_size': args.image_size, 'min_scale': -0.5, 'max_scale': 0.2, 'do_flip': True})
-            train_dataset = 100*sintel_clean + 100*sintel_final + 200*kitti + 5*hd1k + things
+            # train_dataset = 100*sintel_clean + 100*sintel_final + 200*kitti + 5*hd1k + things
+            train_dataset = 100*sintel_clean + 100*sintel_final
+
 
         elif TRAIN_DS == 'C+T+K/S':
-            train_dataset = 100*sintel_clean + 100*sintel_final + things
+            # train_dataset = 100*sintel_clean + 100*sintel_final + things
+            train_dataset = 100*sintel_clean + 100*sintel_final
 
     elif args.stage == 'kitti':
         aug_params = {'crop_size': args.image_size, 'min_scale': -0.2, 'max_scale': 0.4, 'do_flip': False}
