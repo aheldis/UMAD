96cb449bcf3c786dd803f676cae3bb88aed666f2
Haniyeh Ehsani <45337101+aheldis@users.noreply.github.com>
2026-03-03 21:21:37 -0800

    resize to 496

--- noisy_evaluate.py
+++ noisy_evaluate.py
@@ -418,7 +418,7 @@ if __name__ == '__main__':
 
     parser.add_argument('--print_freq', type=int, default=500)
 
-    parser.add_argument('--max_side', type=int, default=640,
+    parser.add_argument('--max_side', type=int, default=496,
                     help='Resize so max(H,W)<=max_side for eval. 0 disables.')
 
     args = parser.parse_args()
